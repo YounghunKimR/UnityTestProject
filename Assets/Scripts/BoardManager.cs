dbd317dfb87df11cd08107742a9572aa53330f8a
YHDev <devyounghun@gmail.com>
2022-07-22 03:47:08 +0900

    GameManager And Ignore list updated

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System;
 using System.Collections.Generic;       //Allows us to use Lists.
 using Random = UnityEngine.Random;      //Tells Random to use the Unity Engine random number generator.
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,32 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+    
+    public BoardManager boardScript;
+
+    private int level = 3;
     // Start is called before the first frame update
-    void Start()
+
+    void Awake()
     {
-        
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
     }
 
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+    }
     // Update is called once per frame
     void Update()
     {
